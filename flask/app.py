759f5e4781750b18247455bfe209d8c40fab29a2
michaelclevenger <clevengermichael93@yahoo.com>
2021-05-24 08:42:35 -0500

    'flask-commit'

--- flask/app.py
+++ flask/app.py
@@ -1,12 +1,42 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-  return 'Clevenger Capstone'
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(144), unique=False)
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
 
 
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('email', 'password')
+
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+@app.route('/user', methods=['POST'])
+def add_user():
+  email = request.json['email']
+  password = request.json['password']
+  new_user = User(email, password)
+  db.session.add(new_user)
+  db.session.commit()
+  user = User.query.get(new_user.id)
+  return user_schema.jsonify(user)
+  
 if __name__ == '__main__':
-  app.run(debug=True)
-
+  app.run(debug=True, port=5000)
